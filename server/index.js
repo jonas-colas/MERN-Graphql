b1e0a5067045e67b47e098f6e871115cfdce990d
jonascolas <jonas@fusemachines.com>
2022-06-28 21:59:10 -0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const colors = require('colors');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
+}));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, console.log(`Server running on port ${port}`));
